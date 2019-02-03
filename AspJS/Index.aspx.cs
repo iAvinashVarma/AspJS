3c706f1799697ef375000a72c8a56a0b361395ec
iAvinashVarma <avinashvarma@live.com>
2019-02-03 14:36:45 +0800

    RegisterStartupScript method without Script tag.

--- AspJS/Index.aspx.cs
+++ AspJS/Index.aspx.cs
@@ -8,8 +8,8 @@ namespace AspJS
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			var key = "clientScript";
-			var script = "<script type=\"text/javascript\">document.getElementById('LblDate').textContent = new Date();</script>";
-			ClientScript.RegisterStartupScript(LblDate.GetType(), key, script);	
+			var script = "document.getElementById('LblDate').textContent = new Date();";
+			ClientScript.RegisterStartupScript(LblDate.GetType(), key, script, true);	
 		}
 	}
 }
