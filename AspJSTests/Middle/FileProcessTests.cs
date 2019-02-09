539e089c540b9d18cf6dbfed846813f15fc3d8a1
iAvinashVarma <avinashvarma@live.com>
2019-02-09 15:33:46 +0800

    Adding FileProcess test and updating the build status specific to master.

---
+++ AspJSTests/Middle/FileProcessTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace AspJS.Middle.Tests
+{
+	[TestClass()]
+	public class FileProcessTests
+	{
+		[TestMethod()]
+		public void FileProcessDataTest()
+		{
+			// Act
+			var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+			var fileProcess = new FileProcess(baseDirectory, "http://avi.com:8080/");
+			var fileData = fileProcess.FileData;
+
+			// Assert
+			Assert.IsTrue(fileData.Any());
+		}
+	}
+}
